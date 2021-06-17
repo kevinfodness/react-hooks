0e5da0573cf92a0b1426b57255ac47e41f1a9026
kevinfodness <2650828+kevinfodness@users.noreply.github.com>
2021-06-17 12:46:41 -0400

    Exercise 2 extra credit 4

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,24 +4,30 @@
 import * as React from 'react'
 
 const useLocalStorageState = (key, initialValue) => {
-  const [stateValue, setStateValue] = React.useState(() => window.localStorage.getItem(key) || initialValue);
+  const [stateValue, setStateValue] = React.useState(() => JSON.parse(window.localStorage.getItem(key)) || initialValue);
   React.useEffect(() => {
-    window.localStorage.setItem(key, stateValue);
+    window.localStorage.setItem(key, JSON.stringify(stateValue));
   }, [key, stateValue]);
   return [stateValue, setStateValue];
 }
 
 function Greeting({initialName = ''}) {
-  const [name, setName] = useLocalStorageState('name', initialName);
+  const [name, setName] = useLocalStorageState('username', initialName);
+  const [age, setAge] = useLocalStorageState('age', 0);
 
-  function handleChange(event) {
+  function handleChangeName(event) {
     setName(event.target.value)
+  }
+  function handleChangeAge(event) {
+    setAge(parseInt(event.target.value, 10))
   }
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChangeName} id="name" />
+        <label htmlFor="age">Age: </label>
+        <input value={age} onChange={handleChangeAge} id="age" type="number" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
